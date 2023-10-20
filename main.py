640305b30ed2409eb9bc2346d8c9d2ce98a4645a
LiyuGT <liyutenaw@gmail.com>
2023-10-19 23:51:26 -0400

    login/register/home skeletons

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello Habit Hero!"
+    return render_template('home.html')
+
+@app.route('/register', methods=['POST'])
+def registerpage():
+    return render_template('register.html')
+
+@app.route('/login', methods=['POST'])
+def loginpage():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
